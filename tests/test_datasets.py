1cda0bcbc13cb66fc5754dcf3e1eff96762d470b
fernandezfran <fernandezfrancisco2195@gmail.com>
2021-09-23 16:32:50 -0300

    test de ejemplo para discutir en la reunión

---
+++ tests/test_datasets.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import pystacho
+
+
+# este test es muy lento para nuestro caso que la base de datos es grande
+# como hacemos para mejorar?
+def test_load_mpdb():
+
+    result = pystacho.datasets.load_mpdb()
+
+    assert isinstance(result, pd.DataFrame)
