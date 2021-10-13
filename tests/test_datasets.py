68af8862018f37302eb3704576196fb36e84e30a
fernandezfran <fernandezfrancisco2195@gmail.com>
2021-10-13 10:43:09 -0300

    test pystacho.datasets.load_mpdb_filter()

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#
+# la primera vez que se corre es muy lento, después queda cacheado
+# quizás haya una forma mejor de hacerlo ...
 
 import numpy as np
 import pandas as pd
@@ -6,8 +9,6 @@ import pandas as pd
 import pystacho.datasets
 
 
-# la primera vez que se corre es muy lento, después queda cacheado
-# quizás haya una forma mejor de hacerlo ...
 def test_load_mpdb():
 
     result = pystacho.datasets.load_mpdb()
@@ -19,6 +20,23 @@ def test_load_mpdb():
     )
     np.testing.assert_almost_equal(
         result["formation_energy_per_atom"].mean(), -1.374002, 6
+    )
+    assert (
+        result["e_above_hull"] >= np.zeros(len(result["e_above_hull"]))
+    ).any()
+
+
+def test_load_mpdb_filter():
+
+    result = pystacho.datasets.load_mpdb_filter()
+
+    assert isinstance(result, pd.DataFrame)
+    np.testing.assert_almost_equal(result["energy"].mean(), -165.758806, 6)
+    np.testing.assert_almost_equal(
+        result["energy_per_atom"].mean(), -5.795536, 6
+    )
+    np.testing.assert_almost_equal(
+        result["formation_energy_per_atom"].mean(), -1.420426, 6
     )
     assert (
         result["e_above_hull"] >= np.zeros(len(result["e_above_hull"]))
